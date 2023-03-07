9b20985054ae3058995f7e2fda979aa308148f57
Marcel Hugo <37884743+marcelhugo70@users.noreply.github.com>
2023-03-07 09:07:47 -0300

    Lista 1 de exercícios

--- lineares/ListaEstatica.java
+++ lineares/ListaEstatica.java
@@ -43,4 +43,51 @@ public class ListaEstatica {
 			tamanho--;
 		}
 	}
+	
+	public String exibir() {
+		String str = "[";
+		for (int i=0; i < tamanho; i++) {
+			str += info[i]+", ";
+		}
+		return str+"]";
+	}
+	
+	public ListaEstatica copiar() {
+		ListaEstatica outra = new ListaEstatica();
+		for (int i=0; i < tamanho; i++) {
+			outra.inserir(info[i]);
+		}
+		return outra;
+	}
+	
+	public int getTamanho() {
+		return this.tamanho;
+	}
+	
+	public int pegar(int pos) {
+		if (pos <0 || pos >= tamanho) {
+			throw new IndexOutOfBoundsException("Pos = "+pos);
+		}
+		return info[pos];
+	}
+	
+	public void concatenar(ListaEstatica outra) {
+		for (int i=0; i < outra.getTamanho(); i++) {
+			this.inserir(outra.pegar(i));
+		}
+	}
+	
+	public boolean estaVazia() {
+		return (tamanho == 0);
+	}
+	
+	public ListaEstatica dividir() {
+		ListaEstatica dividida = new ListaEstatica();
+		int metade = this.tamanho / 2;
+		for (int i = metade; i < tamanho; i++) {
+			dividida.inserir(info[i]);
+		}
+		this.tamanho = metade;
+		return dividida;
+	}
 }
